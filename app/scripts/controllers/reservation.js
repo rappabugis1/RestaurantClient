afff11a47ab29fde402f5db703e5b3c5fd29b311
Ridvan <rappabugis1@etf.unsa.ba>
2018-11-07 23:32:05 +0100

    Fixed issue with date picker and time picker, now you cant todays time if its over working hours- set to 23:00

--- app/scripts/controllers/reservation.js
+++ app/scripts/controllers/reservation.js
@@ -10,16 +10,32 @@
 angular.module('clientApp')
   .controller('ReservationController', function ($scope) {
     $scope.dateToday= new Date();
-    $scope.dateMax = new Date().setMonth($scope.dateToday.getMonth()+4);
-    $scope.selectedDate =$scope.dateToday;
 
+    $scope.maxDate = new Date().setMonth($scope.dateToday.getMonth()+4);
+    $scope.minDate=new Date();
+    $scope.selectedDate =new Date();
+
+    $scope.minTime = new Date().setHours(9, 0, 0);
+
+    if ($scope.selectedDate.getDate() === $scope.dateToday.getDate() && new Date() <= new Date().setHours(21,0,0) ) {
+      $scope.minTime = new Date().setHours(new Date().getHours() + 1, 0, 0);
+    } else {
+      $scope.minTime = new Date().setHours(9, 0, 0);
+      $scope.minDate.setDate($scope.dateToday.getDate() + 1);
+      $scope.selectedDate.setDate($scope.dateToday.getDate() + 1);
+    }
 
     $scope.$watch('selectedDate', function(){
-      if($scope.selectedDate.getDate() === $scope.dateToday.getDate()){
-        $scope.minTime=new Date().setHours($scope.selectedDate.getHours()+1,0,0);
-      } else {
-        $scope.minTime=new Date().setHours(9,0,0);
+      if($scope.selectedDate!==undefined) {
+        if ($scope.selectedDate.getDate() === $scope.dateToday.getDate() && new Date() <= new Date().setHours(21,0,0) ) {
+          $scope.minTime = new Date().setHours(new Date().getHours() + 1, 0, 0);
+        } else {
+          $scope.minTime = new Date().setHours(9, 0, 0);
+          $scope.minDate.setDate($scope.dateToday.getDate() + 1);
+        }
       }
     });
+
+    $scope.numberPeople = [1,2,3,4,5,6,7,8,9,10];
 
   });
