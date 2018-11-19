098214a95c7e2af03d8bc3210ac60ba9da70a893
Ridvan <rappabugis1@etf.unsa.ba>
2018-11-19 02:11:05 +0100

    heroku work again

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -31,6 +31,7 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/ngstorage/ngStorage.js',
+      'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       // endbower
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
